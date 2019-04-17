6c6fdb47a8afbd0c9f6287c9709af7ad05acc08d
crazyball <774028406@qq.com>
2019-04-17 22:19:26 +0800

    修改自动部署

--- auto-depoly/app.js
+++ auto-depoly/app.js
@@ -12,35 +12,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // 自动部署前端代码
 app.post('/front-source/', (req, res) => {
+  console.log('========================');
   let cmds = [
     `cd ${config.FRONT_SOURCE_PATH}`,
+    `git checkout .`, // 本地新增了一堆文件(并没有git add到暂存区)，想放弃修改。
+    `git clean -xdf`, // 本地修改/新增了一堆文件，已经git add到暂存区，想放弃修改。
     `git pull`
   ];
   let code = shelljs.exec(cmds.join(' && ')).code;
   if (code !== 0) {
-    console.log('自动部署失败！');
+    console.log('fail auto depoly！');
     res.send({ msg: 'Fail' });
   } else {
-    console.log('前端代码自动部署成功！');
+    console.log('front-source success auto depoly');
     res.send({ msg: 'Success' });
   }
+  console.log('========================');
 });
 
 // 自动部署后端代码
 app.post('/back-source/', (req, res) => {
+  console.log('========================');
   let cmds = [
     `cd ${config.BACK_SOURCE_PATH}`,
+    `git checkout .`, // 本地新增了一堆文件(并没有git add到暂存区)，想放弃修改。
+    `git clean -xdf`, // 本地修改/新增了一堆文件，已经git add到暂存区，想放弃修改。
     `git pull`
   ];
   let code = shelljs.exec(cmds.join(' && ')).code;
   if (code !== 0) {
-    console.log('自动部署失败！');
+    console.log('fail auto depoly！');
     res.send({ msg: 'Fail' });
   } else {
-    console.log('后端代码自动部署成功！');
+    console.log('back-source success auto depoly');
     res.send({ msg: 'Success' });
   }
-
+  console.log('========================');
 });
 
 app.listen(3001, (err) => {
