a717e70e2c93d636959ab4d2e83b2fbdd543da37
sujiahao <sujiahao@efun.com>
2019-10-25 12:04:22 +0800

    test

--- auto-depoly/user-module.sh
+++ auto-depoly/user-module.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd "/root/crazyball/source_code/Go-project/src/user-module"
-git checkout .
-git pull
-go build -o user-module main.go
+cd "/root/crazyball/source_code/Go-project/src/user-module" && 
+git checkout . &&
+git pull &&
+/usr/local/go/bin/go build -o user-module main.go &&
 pm2 restart user-module
