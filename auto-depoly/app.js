1d5bde58d00dd239cb8c6ca4cab40c1f408b5390
crazyball <774028406@qq.com>
2019-11-03 17:51:15 +0800

    添加前端代码部署

--- auto-depoly/app.js
+++ auto-depoly/app.js
@@ -10,40 +10,49 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// 前端
+app.post('/front-source', async (req, res) => {
+  let projectPath = config["front-source"]
+  let cmds = [
+    `cd ${projectPath}`,
+    `git checkout .`,
+    `git pull`,
+    `cp -rf ${projectPath}/static/. ${config["static-path"]}`
+  ];
+  let shellRes = shelljs.exec(cmds.join(' && '));
+  if (shellRes.code != 0) {
+    res.send({ msg: 'Fail' });
+  } else {
+    res.send({ msg: 'Success' });
+  }
+  let result = await sendEmail("front-source", shellRes.code == 0 ? true : false, shellRes.code == 0 ? shellRes.stdout : shellRes.stderr);
+  console.log(`Send Email:${result.data.status}`);
+})
 
 // 自动部署node代码
 app.post('/node/:name', async (req, res) => {
-  console.log('========================');
   let projectName = req.params.name
   let data = depolyNode(projectName)
   if (!data.res) {
-    console.log('fail auto depoly！');
     res.send({ msg: 'Fail' });
   } else {
-    console.log('user-module success auto depoly');
     res.send({ msg: 'Success' });
   }
-  let result = await sendEmail('user-module', data.res, data.msg);
+  let result = await sendEmail(projectName, data.res, data.msg);
   console.log(`Send Email:${result.data.status}`);
-  console.log('========================');
 });
 
 // 自动部署go代码
-app.post('/go/:name',async (req,res)=>{
-  console.log('========================');
+app.post('/go/:name', async (req, res) => {
   let projectName = req.params.name
   let data = depolyGo(projectName)
   if (!data.res) {
-    console.log('fail auto depoly！');
     res.send({ msg: 'Fail' });
   } else {
-    console.log('user-module success auto depoly');
     res.send({ msg: 'Success' });
   }
-  let result = await sendEmail('user-module', data.res, data.msg);
+  let result = await sendEmail(projectName, data.res, data.msg);
   console.log(`Send Email:${result.data.status}`);
-  console.log('========================');
 })
 
 app.listen(3001, (err) => {
@@ -51,11 +60,11 @@ app.listen(3001, (err) => {
   console.log('自动部署服务已开启...');
 })
 
-async function sendEmail(projectName, isSuccess,stdout) {
+async function sendEmail(projectName, isSuccess, stdout) {
   let arr = stdout.split("\n");
-  arr.splice(1,1);
+  arr.splice(1, 1);
   let detail;
-  for(let i=0;i<arr.length;i++){
+  for (let i = 0; i < arr.length; i++) {
     detail += `<p>${arr[i]}</p>`
   }
   // console.log(detail)
@@ -68,12 +77,12 @@ async function sendEmail(projectName, isSuccess,stdout) {
   return res;
 }
 
-function depolyGo(projectName){
+function depolyGo(projectName) {
   let projectPath = config[projectName];
-  if(!projectPath) {
+  if (!projectPath) {
     return {
-      res:false,
-      msg:"项目路径不存在",
+      res: false,
+      msg: "项目路径不存在",
     }
   };
   let cmds = [
@@ -86,26 +95,26 @@ function depolyGo(projectName){
   let shellRes = shelljs.exec(cmds.join(' && '));
   let code = shellRes.code;
   let out = `===[Error]===\n${shellRes.stderr}\n===[INFO]===\n${shellRes.stdout}`
-  if(code!==0){
+  if (code !== 0) {
     return {
-      res:false,
-      msg:out,
+      res: false,
+      msg: out,
     }
-  }else{
+  } else {
     let status = getPM2Status()
     return {
-      res:true,
-      msg:`${out}\n${status}`,
+      res: true,
+      msg: `${out}\n${status}`,
     }
   }
 }
 
-function depolyNode(projectName){
+function depolyNode(projectName) {
   let projectPath = config[projectName];
-  if(!projectPath) {
+  if (!projectPath) {
     return {
-      res:false,
-      msg:"项目路径不存在",
+      res: false,
+      msg: "项目路径不存在",
     }
   };
   let cmds = [
@@ -117,30 +126,30 @@ function depolyNode(projectName){
   let shellRes = shelljs.exec(cmds.join(' && '));
   let code = shellRes.code;
   let out = `===[Error]===\n${shellRes.stderr}\n===[INFO]===\n${shellRes.stdout}`
-  if(code!==0){
+  if (code !== 0) {
     return {
-      res:false,
-      msg:out,
+      res: false,
+      msg: out,
     }
-  }else{
+  } else {
     let status = getPM2Status()
     return {
-      res:true,
-      msg:`${out}\n${status}`,
+      res: true,
+      msg: `${out}\n${status}`,
     }
   }
 }
 
-function getPM2Status(){
+function getPM2Status() {
   let shellRes = shelljs.exec(`pm2 jlist`);
-  if(shellRes.code!=0){
+  if (shellRes.code != 0) {
     return shellRes.stderr
   }
   let html = "===[Status]===\n<table border=1>"
   let list = JSON.parse(shellRes.stdout)
   html += "<tr><th>id(pid)</th><th>name</th><th>monit</th><th>status</th><th>restart_time</th></tr>"
   list.forEach(task => {
-    html += `<tr><th>${task.pm_id}(${task.pid})</th><th>${task.name}</th><th>mem:${parseInt(task.monit.memory/1024/1024)}M cpu:${task.monit.cpu}</th><th>${task.pm2_env.status}</th><th>${task.pm2_env.restart_time}</th></tr>` 
+    html += `<tr><th>${task.pm_id}(${task.pid})</th><th>${task.name}</th><th>mem:${parseInt(task.monit.memory / 1024 / 1024)}M cpu:${task.monit.cpu}</th><th>${task.pm2_env.status}</th><th>${task.pm2_env.restart_time}</th></tr>`
   });
   html += "</table>"
   return html
--- auto-depoly/config.js
+++ auto-depoly/config.js
@@ -1,5 +1,6 @@
 module.exports = {
+  "static-path": '/root/crazyball/static',
   "front-source": '/root/crazyball/source_code/front-source',
   "back-source": '/root/crazyball/source_code/back-source',
-  "user-module":'/root/crazyball/source_code/Go-project/src/user-module',
+  "user-module": '/root/crazyball/source_code/Go-project/src/user-module',
 }
